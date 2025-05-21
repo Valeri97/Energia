0b7a440a3ffcbab163b2e20af3ebc57827e55331
Valeri97 <113702521+Valeri97@users.noreply.github.com>
2025-05-21 17:27:42 +0200

    modificaciones formiulario

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ if (scrollPosition) {
     }
 });
 
-        /* Seccion inicio - boton/imagen/texto */
+        /* Header */
+const btnMenu = document.getElementById("btnMenu");
+const menu = document.getElementById("menuPrincipal");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+        /* Seccion2 inicio - boton/imagen/texto */
 function MostrarTexto(textoId, mostrar) {
     const texto = document.getElementById(textoId);
     const seccion = document.getElementById("inicio");
@@ -34,5 +42,55 @@ function MostrarTexto(textoId, mostrar) {
         texto.style.display = "none";
         seccion.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('img/imagenPrincipal-inicio.jpg')";
     }
-    
 }
+
+        /* Seccion1 instroducción - cifras */
+function cambiarCifra() {
+  const cifras = [
+    { valor: "15,4", unidad: "GW", texto: "Capacidad instalada total a diciembre 2024" },
+    { valor: "13.6", unidad: "GW", texto: "Capacidad instalada consolidada a Diciembre 2024" },
+    { valor: "26,7", unidad: "TWh", texto: "Produccion total a Diciembre 2024" },
+    { valor: "24", unidad: "paises", texto: "Estamos presentes en los 5 continentes" },
+    { valor: "14,4", unidad: "M", texto: "Toneladas de CO2 evitadas a Diciembre 2024" },
+    { valor: "6,4", unidad: "M", texto: "Personas equivalentes abastecidas" }
+  ];
+
+  let index = 0;
+
+  const valorSpan = document.querySelector(".cifras .valor");
+  const unidadSpan = document.querySelector(".cifras .unidad");
+  const textoP = document.querySelector(".cifras .textoCifras");
+
+  const btnAnterior = document.querySelector(".btn-anterior");
+  const btnSiguiente = document.querySelector(".btn-siguiente");
+
+  function actualizarCifra(nuevoIndex) {
+    // Animación salida
+    valorSpan.style.opacity = 0;
+    unidadSpan.style.opacity = 0;
+    textoP.style.opacity = 0;
+
+    setTimeout(() => {
+      index = (nuevoIndex + cifras.length) % cifras.length; // Para que no pase el límite
+
+      valorSpan.textContent = cifras[index].valor;
+      unidadSpan.textContent = cifras[index].unidad;
+      textoP.textContent = cifras[index].texto;
+
+      // Animación entrada
+      valorSpan.style.opacity = 1;
+      unidadSpan.style.opacity = 1;
+      textoP.style.opacity = 1;
+    }, 300);
+  }
+
+  btnSiguiente.addEventListener("click", () => {
+    actualizarCifra(index + 1);
+  });
+
+  btnAnterior.addEventListener("click", () => {
+    actualizarCifra(index - 1);
+  });
+}
+
+cambiarCifra();
